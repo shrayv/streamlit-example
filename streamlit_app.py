0ab60c624b2f9f76313476bbe43213be8d70cab6
shrayv <137474334+shrayv@users.noreply.github.com>
2023-06-27 17:24:41 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,25 +22,16 @@ header = {'User-Agent':'YourEmail@YourDomain.com'}
 response = requests.get(listurl, headers=header)
 
 
+# github token
+token = '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'
+headers = {'Authorization' : 'token ' + token }
+url = 'https://raw.githubusercontent.com/shrayv/test/main/company_tickers.json?token=GHSAT0AAAAAACEIPXINF56ZV2F2IUESRYTKZE2YJAA'
+response = requests.get(url, headers=header)
+companylist=pd.json_normalize(list(response.json().values()))
 
 
-with st.echo(code_location='below'):
-    total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
-    num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
+option = st.selectbox(
+    'Select Company',
+    (data['ticker']))
 
-    Point = namedtuple('Point', 'x y')
-    data = []
-
-    points_per_turn = total_points / num_turns
-
-    for curr_point_num in range(total_points):
-        curr_turn, i = divmod(curr_point_num, points_per_turn)
-        angle = (curr_turn + 1) * 2 * math.pi * i / points_per_turn
-        radius = curr_point_num / total_points
-        x = radius * math.cos(angle)
-        y = radius * math.sin(angle)
-        data.append(Point(x, y))
-
-    st.altair_chart(alt.Chart(pd.DataFrame(data), height=500, width=500)
-        .mark_circle(color='#0068c9', opacity=0.5)
-        .encode(x='x:Q', y='y:Q'))
+st.write('You selected:', option)
